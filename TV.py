1f1237a0cfa54d4fd5160ad00ab44fdcde82635b
Gene014 <calmaeugene06@gmail.com>
2023-05-14 23:27:23 +0800

    creating pseudocode

---
+++ TV.py
@@ -0,0 +1,42 @@
+#Calma, Eugene Marie S.
+
+# transferring uml diagram into text (serves as my guide)
+
+#tv1's channel is 30 and volume level is 3 
+#tv2's channel is 3 and volume level is 2 
+#channel: int 
+#volumeLevel: int 
+#on: bool 
+
+
+# TV()
+# turnOn (): None
+# turnoff(): None
+# getChannel(): int
+#setChannel (channel: int): None
+#getVolume (): int
+#setVolume (volume Level: int): None
+#channe1Up(): None
+#channelDown (): None
+#volumeUp(): None
+#volumeDown (): None
+#The current channel (1 to 120) of this TV. 
+#The current volume level (1 to 7) of this TV. 
+#Indicates whether this TV is on/off. 
+#Constructs a default TV object.  
+#Turns on this TV.  
+#Turns off this TV.    
+#Returns the channel for this TV. 
+#Sets a new channel for this TV. 
+#Gets the volume level for this TV. 
+#Sets a new volume level for this TV. 
+#Increases the channel number by 1. 
+#Decreases the channel number by 1. 
+#Increases the volume level by 1. 
+#Decreases the volume level by 1.      
+
+#Creating Pseudocode
+#Generating Class
+#Creating Constructor
+#Creating Instances
+#Call Methods
